6ddd9fe7c8f75512caca77790e2f48a08cd6212c
ahmad <ahmadsb1994@gmail.com>
2021-11-29 11:44:03 +0200

    insert test function of shortener object __str__

--- compacturl/tests.py
+++ compacturl/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Shortener
 
 # Create your tests here.
+class ShortenerModelTestcase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Shortener.objects.create(long_url="https://github.com/ahmadsb/CompactURL#activate-the-virtual-environment")
+
+
+    def test_string_method(self):
+        shortener = Shortener.objects.get(id=1)
+      
+        expected_string = f"compact: {shortener.long_url} to {shortener.short_url}"
+        self.assertEqual(str(shortener), expected_string)
+
